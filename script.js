38147baa33d5af946b347ad64719c02f68f703d8
cassidysch <123706941+cassidysch@users.noreply.github.com>
2024-05-07 03:15:33 +0000

    more changes and adding media

--- script.js
+++ script.js
@@ -3,16 +3,28 @@ let about = document.querySelector(".about");
 let responseOne = document.querySelector(".firstresponsepage");
 let aboutButton = document.querySelector(".aboutbutton");
 let responseOneButton = document.querySelector(".response1button");
+let backHomeButton = document.querySelector(".backhome");
+let backHomeButton2 = document.querySelector(".backhome2")
 
 about.style.display = "none";
 responseOne.style.display = "none";
 
 aboutButton.onclick = function() {
-    home.style.display = "none";
-    about.style.display = "block";
+  home.style.display = "none";
+  about.style.display = "flex";
 };
 
 responseOneButton.onclick = function() {
-    home.style.display = "none";
-    responseOne.style.display = "block";
+  home.style.display = "none";
+  responseOne.style.display = "block";
+};
+
+backHomeButton.onclick = function() {
+  about.style.display = "none"; 
+  home.style.display = "flex";
+};
+
+backHomeButton2.onclick = function() {
+  responseOne.style.display = "none";
+  home.style.display = "flex";
 };
