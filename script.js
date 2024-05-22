2015a0b499d2f83cbe508a8ef0af70fca702a34f
cassidysch <123706941+cassidysch@users.noreply.github.com>
2024-05-22 07:11:49 +0000

    finished artistic response 3

--- script.js
+++ script.js
@@ -6,18 +6,28 @@ let responseOneButton = document.querySelector(".response1button");
 let backHomeButton = document.querySelector(".backhome");
 let backHomeButton2 = document.querySelector(".backhome2");
 let backHomeButton3 = document.querySelector(".backhome3");
+let backHomeButton4 = document.querySelector(".backhome4");
 let lyricButton = document.querySelector("#lyricbtn");
 let lyrics = document.querySelector("#lyrics");
+
 let responseTwo = document.querySelector(".secondresponsepage");
 let responseTwoButton = document.querySelector(".response2button");
 let lyricButtonTwo = document.querySelector("#lyricbtn2");
 let lyricsTwo = document.querySelector("#lyrics2");
 
+let responseThree = document.querySelector(".thirdresponsepage");
+let responseThreeButton = document.querySelector(".response3button");
+let lyricsButtonThree = document.querySelector("#lyricsbtn3");
+let lyricsThree = document.querySelector("#lyrics3");
+
+
 about.style.display = "none";
 responseOne.style.display = "none";
 responseTwo.style.display = "none";
+responseThree.style.display = "none";
 lyrics.style.display = "none";
 lyricsTwo.style.display = "none";
+lyricsThree.style.display = "none";
 
 aboutButton.onclick = function() {
   home.style.display = "none";
@@ -32,6 +42,11 @@ responseOneButton.onclick = function() {
 responseTwoButton.onclick = function() {
   home.style.display = "none";
   responseTwo.style.display = "block";
+};
+
+responseThreeButton.onclick = function() {
+  home.style.display = "none";
+  responseThree.style.display = "block";
 };
 
 backHomeButton.onclick = function() {
@@ -49,10 +64,19 @@ backHomeButton3.onclick = function() {
   home.style.display = "flex";
 };
 
+backHomeButton4.onclick = function() {
+  responseThree.style.display = "none";
+  home.style.display = "flex";
+};
+
 lyricButton.onclick = function() {
   lyrics.style.display = "block";
 };
 
 lyricButtonTwo.onclick = function() {
   lyricsTwo.style.display = "block";
+};
+
+lyricsButtonThree.onclick = function() {
+  lyricsThree.style.display = "block";
 };
